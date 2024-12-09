1efb4f439d31aa418a03c5812014358b4cc168ad
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2024-12-09 14:14:47 -0300

    feat: crud functionalities

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from .views import *
+from .api import PostSerializer, PostList, PostCreate
+urlpatterns = [
+    path('list', PostList.as_view()),
+    path('detail/<str:pk>', PostDetail),
+    path('create', PostCreate.as_view()),
+    path('update/<str:pk>', PostUpdateView, name='post-update'),
+    path('delete/<str:pk>', PostDeleteView, name='post-delete'),
 
-urlpatterns = []
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .serializer import PostSerializer
+from .models import Post
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def PostListView(request):
+    api_urls = {
+        'all_posts': '/',
+        'add': '/create'
+    }
+
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def PostDetail(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def PostCreateView(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+
+@api_view(['PATCH'])
+def PostUpdateView(request, pk):
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(instance=post, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=200)
+
+
+@api_view(['DELETE'])
+def PostDeleteView(request, pk):
+    post = Post(id=pk)
+    post.delete()
+    return Response(status=204)
